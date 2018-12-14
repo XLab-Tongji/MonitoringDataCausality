0271cf68d80129d133fe6ffd96309ea039fd9b30
tjuyinkanglin <14_ykl@tongji.edu.cn>
2018-12-14 16:24:57 +0800

    formalize run.sh

--- backend/xlab/run.sh
+++ backend/xlab/run.sh
@@ -1,6 +1,25 @@
 #! /bin/bash
+PORT=8080
+#set container port if have "-p" option
+while getopts 'p:' OPT; do
+    case $OPT in
+        p)
+            PORT=$OPTARG;
+    esac
+done
+
+#add data processing libraries
 mvn install:install-file -Dfile=src/lib/tetrad-gui-6.6.0-SNAPSHOT.jar -DgroupId=edu.cmu -DartifactId=tetrad-gui -Dversion=6.6.0-SNAPSHOT -Dpackaging=jar -DgeneratePom=true
 mvn install:install-file -Dfile=src/lib/data-reader-0.2.4.jar -DgroupId=edu.pitt.dbmi -DartifactId=data-reader -Dversion=0.2.4 -Dpackaging=jar -DgeneratePom=true
 mvn package
-docker build -f src/main/docker/Dockerfile -t causalitysearch-v2.0 .
-docker run -d -p 8080:8080 causalitysearch-v2.0
+
+#build docker and run
+
+CURRENT_CONTAINER=$(docker ps -aq -f "name=causality_search")
+if [ -n "$CURRENT_CONTAINER" ]; then
+    docker stop $CURRENT_CONTAINER
+    docker rm $CURRENT_CONTAINER
+fi
+
+docker build -f src/main/docker/Dockerfile -t causalitysearch:2.0 .
+docker run --name causality_search -d -p $PORT:8080 causalitysearch:2.0
