85a21e6629110069070345db14fedc0464a22acb
tjuyinkanglin <14_ykl@tongji.edu.cn>
2019-05-17 22:15:31 +0800

    Clean Unnecessary Files

--- backend/CausalitySearch/run.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-#mvn install:install-file -Dfile=tetrad-gui-6.6.0-SNAPSHOT.jar -DgroupId=edu.cmu -DartifactId=tetrad-gui -Dversion=6.6.0-SNAPSHOT -Dpackaging=jar -DgeneratePom=true
-mvn package
-docker build -f src/main/docker/Dockerfile -t causalitysearch .
-docker run -d -p 8080:8080 causalitysearch
--- backend/CausalitySearch/src/main/java/edu/cmu/tetrad/util/Experimental.java
+++
@@ -1,9 +0,0 @@
-package edu.cmu.tetrad.util;
-
-/**
- * Tags a file as experimental so it's not listed in the configuration.
- *
- * @author jdramsey
- */
-public interface Experimental {
-}
