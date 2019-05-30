ff02d76ed3e2d5e3af9834659c022a3d32ae87c2
tjuyinkanglin <14_ykl@tongji.edu.cn>
2019-05-30 22:44:42 +0800

    modify startup scripts

--- run.sh
+++ run.sh
@@ -8,10 +8,11 @@ while getopts 'p:' OPT; do
     esac
 done
 
-#add data processing libraries
+#add data processing libraries and build package
+cd CausalitySearch
 mvn install:install-file -Dfile=src/lib/tetrad-lib-6.7.0-SNAPSHOT.jar -DgroupId=edu.cmu -DartifactId=tetrad-lib -Dversion=6.7.0-SNAPSHOT -Dpackaging=jar -DgeneratePom=true
 mvn package
-
+cd ..
 #build docker and run
 
 CURRENT_CONTAINER=$(docker ps -aq -f "name=causality_search")
@@ -20,5 +21,5 @@ if [ -n "$CURRENT_CONTAINER" ]; then
     docker rm $CURRENT_CONTAINER
 fi
 
-docker build -f src/main/docker/Dockerfile -t causalitysearch:2.0 .
+docker build -f Docker/Dockerfile -t causalitysearch:2.0 .
 docker run --name causality_search -d -p $PORT:8080 causalitysearch:2.0
