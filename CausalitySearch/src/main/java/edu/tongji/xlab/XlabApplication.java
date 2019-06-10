d97abcdb74ec3ed5a578cf8130ea50e2d51801a7
tjuyinkanglin <14_ykl@tongji.edu.cn>
2019-06-10 16:53:53 +0800

    fix file size limit error

--- CausalitySearch/src/main/java/edu/tongji/xlab/XlabApplication.java
+++ CausalitySearch/src/main/java/edu/tongji/xlab/XlabApplication.java
@@ -2,12 +2,27 @@ package edu.tongji.xlab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+import javax.servlet.MultipartConfigElement;
+
+@Configuration
 @SpringBootApplication
 public class XlabApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(XlabApplication.class, args);
+    }
+
+    @Bean
+    public MultipartConfigElement multipartConfigElement(){
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+
+        factory.setMaxFileSize("50MB");
+        factory.setMaxRequestSize("50MB");
+        return factory.createMultipartConfig();
     }
 
 }
